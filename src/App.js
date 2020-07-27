cfd6cc1c5b665aecbe073160d08fa877582045da
Willjunger <williamjunger@hotmail.com>
2020-07-27 16:14:41 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Menu from "./components/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ background: "#141414" }}>
+			<Menu />
+
+			<BannerMain
+				videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+				url={dadosIniciais.categorias[0].videos[0].url}
+				videoDescription={
+					"Tibia é um jogo eletrônico multijogador (MMORPG) gratuito, desenvolvido pela CipSoft. Criado em 1997, é um dos jogos mais antigos do gênero. Nele, os jogadores podem desenvolver as habilidades de seus personagens, buscar tesouros, resolver enigmas e explorar áreas como cidades, masmorras, florestas, desertos, ilhas, praias, minas, etc.. Os personagens podem disputar lutas entre si ou com criaturas, tais como monstros, dragões, demônios, orcs, utilizando armas e magias, enquanto os NPCs não podem ser atacados."
+				}
+			/>
+
+			<Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+			<Carousel category={dadosIniciais.categorias[1]} />
+
+			<Carousel category={dadosIniciais.categorias[2]} />
+
+			<Carousel category={dadosIniciais.categorias[3]} />
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
